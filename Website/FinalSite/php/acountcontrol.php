4cfaf0d6cc6b385eaae89c9cf65fd98354744dd9
Austin Johnson <austin.127.0.0.1@gmail.com>
2014-03-13 16:55:31 -0400

    Adding some PHP

---
+++ Website/FinalSite/php/acountcontrol.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<body>
+<a href="#loginForm" id="loginBtn">Login</a> | 
+<a href="#joinForm" id="joinBtn">Join</a>
+</body>
+</html>
