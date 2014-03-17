f3c53e2a02fdda6f2881499ccbdea7ddbfebfbd9
mmpetrillose <mmpetrillose@gmail.com>
2014-03-17 05:51:08 -0400

    about.php

---
+++ Website/FinalSite/about.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Ctrl-P</title>
+<link href="css/main.css" rel="stylesheet" type="text/css">
+<link href="css/overlay.css" rel="stylesheet" type="text/css">
+<link href="css/tutorial.css" rel="stylesheet" type="text/css" />
+<script src="js/JQuery1_11_0.js"></script>
+<script src="js/search.js"></script>
+</head>
+<body>
+<div id="wrapper" class="clearfix">
+    <div id="header">
+      <div id="homeDiv" class="inset-text-grey">
+        <a href="index.php" id="home">Ctrl-P</a>
+      </div>
+      <div id="catalogDiv" class="inset-text-grey">
+        <a href="CatalogMain.php" id="catalog">Catalog</a>
+      </div>
+      <div id="tutorialDiv"  class="inset-text-grey">
+        <a href="TutorialMain.php" id="tutorial">Tutorial</a>
+      </div>
+      <div id="search">
+        <input type="text" size="35" placeholder="Search..." />
+      </div>
+      <div id="profile" class="inset-text-grey">
+        <?php include('php/accountcontrol.php');?>
+      </div>
+    </div>
+    <div id="content">
+	<div id="image3d">
+      <img src = "pic1.jpeg" alt "3Dimage">
+     </div>
+	  <div id="image3dText">
+	  <H2>Ctrl-P</h2> Is a repository for 3D printing material. Its a place for beginner and intermediate people to come and
+	  get information to make their 3D printing experience better. We are here to help you with you 3D printing needs.
+	  </div>
+	  
+	  
+        <a href="" class="overlay" id="loginForm"></a>
+        <div class="popup inset-text-white centered" id="login">
+          <?php include('php/loginpop.php');?>
+        </div>
+        <a href="" class="overlay" id="joinForm"></a>
+        <div class="popup inset-text-white centered" id="join">
+          <?php include('php/joinpop.php');?>
+        </div>
+    </div>
+    <div id="footer">
+      <a href="about.php" id="about">About</a> |
+      <a href="help.php" id="help">Help</a> |
+      <a href="contact.php" id="contactUs">Contact Us</a>
+    </div>
+</div>
+</body>
+</html>
